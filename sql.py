60e445db24ca25dff3e1825dba5c381904e18acb
zhao94254 <zhao942548838@qq.com>
2019-04-01 20:58:08 +0800

    table

--- sql.py
+++ sql.py
@@ -4,3 +4,16 @@
 # @Email   : zpy94254@gmail.com
 # @File    : sql.py
 # @Software: PyCharm
+
+from collections import namedtuple
+
+def create_table(row, data):
+    """
+    row[0] 表名  row[1:] 列名 data 要插入的数据
+    :param row: ['Job', 'salary', 'name', 'city']
+    :param data: [[100, dev, cz]]
+    :return:
+    """
+    table_row = namedtuple(row[0], row[1:])
+    table = [table_row(*i) for i in data]
+    return table
